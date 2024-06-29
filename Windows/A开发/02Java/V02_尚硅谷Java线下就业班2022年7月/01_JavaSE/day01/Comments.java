1f3cbe03293ab77449a7fe449540547f29690c57
fengziprogrammer <feng_zi_programmer@163.com>
2024-06-29 21:23:59 +0800

    A开发::02Java::V02_尚硅谷Java线下就业班2022年7月::01_JavaSE::day01

---
+++ Windows/A开发/02Java/V02_尚硅谷Java线下就业班2022年7月/01_JavaSE/day01/Comments.java
@@ -0,0 +1,19 @@
+/*
+文档注释转换为API文档的工具：javadoc.exe
+格式：javadoc xx.java
+例如：javadoc Comments.java
+	  javadoc -author Comments.java
+*/
+/**
+* 文档注释的演示
+* @author chai
+* @since 1.8
+*/
+public class Comments{
+	/**
+	Java程序的入口
+	@param args String[] 命令行参数
+	*/
+	public static void main(String[] args){
+	}
+}
---
+++ Windows/A开发/02Java/V02_尚硅谷Java线下就业班2022年7月/01_JavaSE/day01/DataType.java
@@ -0,0 +1,15 @@
+/*
+Java是有很多数据类型的，主要分为两大类：
+1、基本数据类型（8种），延续C语言
+整数：byte,short,int,long
+小数：float,double  浮点型，不精确
+单字符：char
+布尔型：boolean    只有两个值true/false
+
+2、引用数据类型
+数组
+类：String等
+接口
+枚举
+注解
+*/
