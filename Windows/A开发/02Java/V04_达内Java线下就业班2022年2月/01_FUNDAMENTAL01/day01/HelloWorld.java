c3bc3d82d51cc6f3781bf0c2edd20a6f590a4c7f
fengziprogrammer <feng_zi_programmer@163.com>
2024-06-30 23:15:57 +0800

    A开发::02Java::V04_达内Java线下就业班2022年2月::01_FUNDAMENTAL01::day01

---
+++ Windows/A开发/02Java/V04_达内Java线下就业班2022年2月/01_FUNDAMENTAL01/day01/HelloWorld.java
@@ -0,0 +1,32 @@
+package day01; //声明包day01(楼+单元)
+public class HelloWorld { //声明类HelloWorld(房子)
+	//主方法，为程序的入口(大门口)
+	//程序的执行从main开始，main结束则程序结束
+	public static void main(String[] args) {
+		//1)严格区分大小写
+		//2)所有符号必须都是英文模式的
+		//3)每句话必须以分号结尾
+		//4)println():输出并换行
+		//  print():输出不换行
+		System.out.print("hello"); //双引号中的原样输出
+		System.out.println("world");
+		System.out.println("欢迎大家来到达内");
+		System.out.println("当日事当日毕");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
